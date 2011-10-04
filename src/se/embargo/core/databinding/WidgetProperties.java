88808e315a6ff55fcac03a74510613ec72176051
Mikael Johansson <mikl.johansson@gmail.com>
2011-10-04 22:22:13 +0200

    Added support for Widget.enabled()

--- src/se/embargo/core/databinding/WidgetProperties.java
+++ src/se/embargo/core/databinding/WidgetProperties.java
@@ -5,11 +5,54 @@ import se.embargo.core.databinding.observable.IObservableValue;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class WidgetProperties {
+	public static IValueProperty<View, Boolean> enabled() {
+		return _enabled;
+	}
+
+	public static IValueProperty<TextView, String> text() {
+		return _text;
+	}
+
+	public static IValueProperty<ProgressBar, Integer> progress() {
+		return _progress;
+	}
+
+	public static IValueProperty<ImageView, Uri> imageUri() {
+		return _imageuri;
+	}
+
+	public static IValueProperty<ImageView, Drawable> imageDrawable() {
+		return _imagedrawable;
+	}
+
+	public static IValueProperty<ImageView, Bitmap> imageBitmap() {
+		return _imagebitmap;
+	}
+
+	public static IValueProperty<ImageView, Integer> imageResource() {
+		return _imageresource;
+	}
+
+	private static IValueProperty<View, Boolean> _enabled = new IValueProperty<View, Boolean>() {
+		public IObservableValue<Boolean> observe(final View object) {
+			return new AbstractObservableValue<Boolean>() {
+				public Boolean getValue() {
+					return object.isEnabled();
+				}
+
+				public void setValue(Boolean value) {
+					object.setEnabled(value);
+				}
+			};
+		}
+	};
+
 	private static IValueProperty<TextView, String> _text = new IValueProperty<TextView, String>() {
 		public IObservableValue<String> observe(final TextView object) {
 			return new AbstractObservableValue<String>() {
@@ -83,23 +126,17 @@ public class WidgetProperties {
 		}
 	};
 
-	public static IValueProperty<TextView, String> text() {
-		return _text;
-	}
+	private static IValueProperty<ImageView, Integer> _imageresource = new IValueProperty<ImageView, Integer>() {
+		public IObservableValue<Integer> observe(final ImageView object) {
+			return new AbstractObservableValue<Integer>() {
+				public Integer getValue() {
+					return -1;
+				}
 
-	public static IValueProperty<ProgressBar, Integer> progress() {
-		return _progress;
-	}
-
-	public static IValueProperty<ImageView, Uri> imageUri() {
-		return _imageuri;
-	}
-
-	public static IValueProperty<ImageView, Drawable> imageDrawable() {
-		return _imagedrawable;
-	}
-
-	public static IValueProperty<ImageView, Bitmap> imageBitmap() {
-		return _imagebitmap;
-	}
+				public void setValue(Integer value) {
+					object.setImageResource(value);
+				}
+			};
+		}
+	};
 }
