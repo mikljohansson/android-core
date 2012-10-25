5175fcddf0ea6ffedacdcdf6f5550a9c6d9bb725
Mikael <devnull@localhost>
2012-10-25 20:05:30 +0200

    Added CursorLoader utility adapters

---
+++ src/se/embargo/core/database/CursorLoaderAdapter.java
@@ -0,0 +1,40 @@
+package se.embargo.core.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public abstract class CursorLoaderAdapter extends CursorAdapter implements LoaderManager.LoaderCallbacks<Cursor> {
+	private final int _resid;
+	
+	public CursorLoaderAdapter(SherlockFragmentActivity context, int resid) {
+		super(context, null, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+		_resid = resid;
+        context.getSupportLoaderManager().initLoader(0, null, this);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		swapCursor(data);
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		swapCursor(null);
+	}
+
+	@Override
+	public View newView(Context context, Cursor cursor, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater)parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View view = inflater.inflate(_resid, parent, false);
+		bindView(view, context, cursor);
+		return view;
+	}
+}
---
+++ src/se/embargo/core/database/ObservableCursorLoader.java
@@ -0,0 +1,48 @@
+package se.embargo.core.database;
+
+import se.embargo.core.databinding.observable.WritableValue;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+public class ObservableCursorLoader extends WritableValue<Cursor> implements LoaderManager.LoaderCallbacks<Cursor> {
+	private final Context _context;
+	private final Uri _uri;
+	private final String[] _projection;
+	private final String _selection;
+	private final String[] _selectionArgs;
+	
+	public ObservableCursorLoader(SherlockFragmentActivity context, Uri uri, String[] projection, String selection, String[] selectionArgs) {
+		_context = context;
+		_uri = uri;
+		_projection = projection;
+		_selection = selection;
+		_selectionArgs = selectionArgs;
+		context.getSupportLoaderManager().initLoader(1, null, this);
+	}
+	
+	public ObservableCursorLoader(SherlockFragmentActivity context, Uri uri, String[] projection) {
+		this(context, uri, projection, null, null);
+	}
+	
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		return new CursorLoader(_context, _uri, _projection, _selection, _selectionArgs, null);
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> obj, Cursor value) {
+		if (value.moveToFirst()) {
+			setValue(value);
+		}
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> obj) {}
+}
