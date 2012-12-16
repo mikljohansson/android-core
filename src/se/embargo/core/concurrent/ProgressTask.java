ebfcbd396b83bb458e1270f2e6021c6a2b9b8742
Mikael Johansson <mikl.johansson@gmail.com>
2012-12-16 14:21:10 +0100

    Added cancel and progress support to progress task

--- src/se/embargo/core/concurrent/ProgressTask.java
+++ src/se/embargo/core/concurrent/ProgressTask.java
@@ -2,10 +2,11 @@ package se.embargo.core.concurrent;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.util.Log;
 
-public abstract class ProgressTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+public abstract class ProgressTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> implements DialogInterface.OnCancelListener {
 	private static final String TAG = "ProgressTask";
 	private final ProgressDialog _dialog;
 	
@@ -17,6 +18,7 @@ public abstract class ProgressTask<Params, Progress, Result> extends AsyncTask<P
             _dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
             _dialog.setIndeterminate(true);
             _dialog.setCancelable(false);
+            _dialog.setOnCancelListener(this);
         }
         catch (RuntimeException e) {
         	// This may occur if the task is created from a non-UI thread
@@ -24,6 +26,29 @@ public abstract class ProgressTask<Params, Progress, Result> extends AsyncTask<P
         	throw e;
         }
 	}
+	
+	protected Context getContext() {
+		return _dialog.getContext();
+	}
+	
+	public void setMaxProgress(int maxprogress) {
+        _dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        _dialog.setIndeterminate(false);
+        _dialog.setMax(maxprogress);
+	}
+	
+	public void setCancelable(boolean cancelable) {
+		_dialog.setCancelable(cancelable);
+	}
+
+	/**
+	 * Update the current progress.
+	 * @remark	Must be called on the UI thread.
+	 * @param 	progress	Progress between 0 and maxprogress
+	 */
+    protected void setProgress(int progress) {
+    	_dialog.setProgress(progress);
+    }
 
     protected void onPreExecute() {
 		try {
@@ -46,6 +71,8 @@ public abstract class ProgressTask<Params, Progress, Result> extends AsyncTask<P
     }
     
     protected void onCancelled(Result result) {
+    	Log.i(TAG, "Task was cancelled: " + this);
+
     	if (_dialog.isShowing()) {
     		try {
     			_dialog.dismiss();
@@ -54,5 +81,10 @@ public abstract class ProgressTask<Params, Progress, Result> extends AsyncTask<P
     			Log.w(TAG, "Failed to close dialog", e);
     		}
     	}
+    }
+    
+    @Override
+    public void onCancel(DialogInterface dialog) {
+    	cancel(true);
     }
 }
